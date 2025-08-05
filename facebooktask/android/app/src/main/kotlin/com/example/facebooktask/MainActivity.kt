496170eb92667c5668703820fe8e63eb28913af7
karensamuel <113302396+karensamuel@users.noreply.github.com>
2025-08-05 04:32:21 +0300

    commit

---
+++ facebooktask/android/app/src/main/kotlin/com/example/facebooktask/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.facebooktask
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ facebooktask/lib/main.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+import 'facbookpage.dart';
+
+void main() {
+  runApp(const Facbookpage());
+}
